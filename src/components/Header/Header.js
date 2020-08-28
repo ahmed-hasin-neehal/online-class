3b0b2c2bf42a84ccd57facd7e9d54d09d8ab8d4d
ahmed-hasin-neehal <60386953+ahmed-hasin-neehal@users.noreply.github.com>
2020-08-28 22:27:50 +0600

    final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css'
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt=""/>
-            <nav>
-                <a href="/courses">Courses</a>
-                <a href="/checkout">Checkout Purchase</a>
-                <a href="/manage">Manage Courses</a>
-            </nav>
+
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand>EDUCARE</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#courses">Courses</Nav.Link>
+                    <Nav.Link href="#checkout">Checkout Purchase</Nav.Link>
+                    <Nav.Link href="#manage">Manage Courses</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
         </div>
     );
 };
